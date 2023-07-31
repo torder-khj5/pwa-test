023c6f22e2e7ed1c2f18406ef14e3a20d5fb3e43
Daisy-khj5 <133727991+toder-khj5@users.noreply.github.com>
2023-07-31 16:59:03 +0900

    edit index.html

---
+++ dist/assets/index-12368cf8.js
@@ -0,0 +1,87 @@
+import{n,a as t,c as a,I as $,r as k,j as x,b as r,F as v}from"./index-b62d48e4.js";const C=n.input`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  font-size: ${({fontSize:e})=>(e??"16")+"px"};
+  font-weight: ${({fontWeight:e})=>e??500};
+  color: ${({fontColor:e})=>e??t.black_100};
+  letter-spacing: -0.8px;
+  background: ${({backGroundColor:e})=>e??t.navy_0};
+  border: ${({borderColor:e})=>`1px solid ${e??t.line}`};
+  border-radius: 10px;
+  outline: none;
+  caret-color: ${({fontColor:e})=>e??"#777"};
+
+  :focus {
+    color: ${t.black_100};
+    border: 1px solid ${t.navy_900};
+  }
+
+  ::placeholder {
+    color: #777;
+  }
+
+  :disabled {
+    font-weight: 500;
+    color: #babcc2;
+    background-color: #f5f6f8;
+    border: 1px solid ${t.navy_50};
+  }
+
+  ${({isError:e})=>e&&a`
+      border: 1px solid ${t.error};
+    `}
+`,z=C,I=n.div`
+  position: relative;
+  width: 100%;
+  height: 56px;
+  margin-bottom: 15px;
+`,H=n.label`
+  margin-bottom: 15px;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 100%;
+  color: ${t.black_100};
+  letter-spacing: -0.75px;
+`,S=n.div`
+  position: absolute;
+  top: 50%;
+  right: 18px;
+  display: flex;
+  align-items: center;
+  transform: translate(0, -50%);
+`,b=n($)`
+  margin-left: 14px;
+  cursor: pointer;
+
+  object-fit: fit;
+`;n.span`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  color: ${t.red_500};
+  text-align: right;
+  letter-spacing: -0.8px;
+`;const F=n.span`
+  margin-bottom: 21px;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  color: ${t.error};
+  letter-spacing: -0.75px;
+`;function M({inputType:e,inputValue:i,inputChangeEvent:o,placeholder:s,isShowHideIcon:l,isShowDeleteIcon:c,innerComponent:d,disabled:h,textLabel:g,isError:u,errorMessage:m,..._}){const[p,f]=k.useState(e!=="password"),w=()=>{f(!p)},y=()=>{o({target:{value:""}})};return x(v,{children:[g&&r(H,{htmlFor:"custom-input",children:g}),x(I,{children:[r(z,{type:p?"text":"password",placeholder:s,value:i,onChange:o,disabled:h,isError:u,..._}),x(S,{children:[c&&r(b,{onClick:y,name:"삭제_회색",size:24,"data-testid":"delete-icon"}),l&&r(b,{onClick:w,name:p?"보이기_회색":"가리기_회색",size:26,"data-testid":"show-icon"}),d]})]}),u&&r(F,{children:m})]})}const j={buttons:{size:{lg:{width:"90px",height:"60px",fontSize:"20px",lineHeight:"20px"},md:{width:"79px",height:"44px",fontSize:"16px",lineHeight:"16px"},sm:{width:"76px",height:"39px",fontSize:"15px",lineHeight:"15px"},xs:{width:"73px",height:"34px",fontSize:"14px",lineHeight:"14px"}}},modal:{font:{lg:{lineHeight:"27px"},md:{lineHeight:"24px"}},size:{lg:{width:"400px",height:"318px",margin:"40px"},md:{width:"300px",height:"180px",margin:"30px"}},contents:{lg:{height:"54px"},md:{height:"48px"}},button:{lg:{width:"320px",height:"60px"},md:{width:"240px",height:"44px"}}}},T=n.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+  color: ${({priority:e})=>e==="primary"?t.red_500:t.navy_900};
+  text-align: center;
+  letter-spacing: -1px;
+  border-radius: 10px;
+
+  ${({btnSize:e})=>e&&j.buttons.size[e]}
+
+  ${({btnType:e,priority:i})=>e&&B(e,i)}
+`,B=(e,i)=>{const o=i==="primary";switch(e){case"default":return a({backgroundColor:o?t.red_500:t.navy_900,color:t.white_100,":active":{background:o?t.red_500:t.black_100},":disabled":{background:o?t.red_100:t.navy_30,color:o?t.white_100:t.black_10}});case"line":return a({border:`1px solid ${o?t.red_500:t.navy_30}`,background:t.white_100,":active":{border:`1px solid ${o?t.red_500:t.navy_900}`,background:o?t.red_50:t.navy_30},":disabled":{border:`1px solid ${o?t.red_100:t.navy_50}`,background:o?t.red_50:"#F5F6F8",color:o?t.red_200:"#C2C5CC"}});case"bg":return a({background:o?t.red_50:t.navy_30,":active":{background:o?t.red_100:t.navy_50}})}},P=({label:e,onButtonClick:i,isDisabled:o,className:s,priority:l="primary",btnSize:c="md",btnType:d="default"})=>r(T,{type:"button",onClick:()=>{i()},disabled:o,priority:l,btnSize:c,btnType:d,className:s,children:e});export{M as C,P as a,j as t};
---
+++ dist/assets/index-39074fdf.js
@@ -0,0 +1,81 @@
+import{n as e,c as a,a as r,I as h,r as p,j as s,b as o}from"./index-b62d48e4.js";const g=e.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`,f=e.div`
+  margin: 100px 0 50px;
+  font-size: 20px;
+  font-weight: bold;
+`,m=e.div`
+  width: 620px;
+  height: max-content;
+`,b=e.div`
+  position: relative;
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+
+  width: 100%;
+  height: 160px;
+  margin-bottom: 20px;
+
+  background: #fff;
+  border: ${({active:i})=>i?a`
+          border-radius: 20px;
+          border: 2px solid ${r.red_500};
+          background: #fff;
+          box-shadow: 3px 3px 20px 0 rgb(255 67 67 / 20%);
+        `:"1px solid lightgray"};
+  border-radius: 20px;
+  box-shadow: 3px 3px 15px 0 rgb(48 54 68 / 5%);
+
+  :hover {
+    border: 2px solid ${({active:i})=>i?"#ff4545":"#303644"};
+    box-shadow: 3px 3px 3px 20px 0 rgb(71 89 132 / 40%);
+  }
+
+  :active {
+    border: 2px solid ${r.red_500};
+    border-radius: 20px;
+    box-shadow: 3px 3px 20px 0 rgb(255 67 67 / 20%);
+  }
+`,y=e.img`
+  flex-shrink: 0;
+  width: 90px;
+  height: 90px;
+  margin: 35px 0 35px 45px;
+  background: ${r.navy_30};
+  border: 1px solid ${r.navy_50};
+
+  border-radius: 15px;
+`,u=e.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  height: 62px;
+  margin-left: 30px;
+`,v=e.div`
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 100%; /* 28px */
+  color: #000;
+  letter-spacing: -1.4px;
+`,w=e.div`
+  margin-top: 10px;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 100%;
+
+  color: #000;
+  letter-spacing: -1px;
+`,T=e(h)`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  width: 32px;
+  height: 32px;
+`,I=[{label:"개인회원",desc:"만 14세 이상 개인 회원가입",src:"img-src",href:"individual"},{label:"사업자회원",desc:"개인/법인 사업자 또는 사업자 직원 회원가입",src:"img-src",href:"business"},{label:"임직원·협력업체 회원",desc:"임직원 또는 협력업체 회원가입",src:"img-src",href:"employees"}];function _(){const[i,c]=p.useState(""),x=p.useCallback((t,n)=>{console.log(`history.push(${t})`),c(n)},[]);return s(g,{children:[o(f,{children:"가입할 회원 유형을 선택해 주세요."}),o(m,{children:I.map(({label:t,desc:n,src:d,href:l})=>s(b,{onClick:()=>x(l,t),active:i===t,children:[o(y,{src:d}),s(u,{children:[o(v,{children:t}),o(w,{children:n})]}),i===t&&o(T,{name:"체크_흰색_바탕_빨간색"})]},"type-"+t))})]})}export{_ as default};
