6f20d50d552195baabbbac237a79c232d9962923
Daisy-khj5 <133727991+toder-khj5@users.noreply.github.com>
2023-11-02 12:54:38 +0900

    [edit] lint error

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,6 @@
 import { colors } from '@styles/colors.ts';
 import * as S from './styles.tsx';
+import { logger } from 'workbox-core/_private';
 import PouchDB from 'pouchdb';
 
 type Img = {
@@ -9,13 +10,73 @@ type Img = {
 
 export default function HomePage() {
   // const { goods } = useInitSelector(['goods']);
-  const handleClickEvent = () => {
-    console.log('handleClick!');
-    try {
-      // const db = new PouchDB('http://192.168.20.227:5984/dbname');
-      const db = new PouchDB('test'); // 로컬 PouchDB 데이터베이스 생성
-      const remoteDB = new PouchDB('http://admin:0000@192.168.0.16:5984/test');
+  // const handleClickEvent = () => {
+  //   console.log('handleClick!');
+  //   try {
+  //     const db = new PouchDB('test'); // 로컬 PouchDB 데이터베이스 생성
+  //     const remoteDB = new PouchDB('http://admin:0000@192.168.0.14:5984/test');
+  //
+  //     // DB 연결 및 동기화
+  //     db.sync(remoteDB, {
+  //       live: true,
+  //       retry: true,
+  //     });
+  //
+  //     db.changes().on('change', (change) => {
+  //       console.log('로컬 데이터베이스 변경 감지:', change);
+  //     });
+  //
+  //     // 데이터 추가 (Create)
+  //     const doc = {
+  //       _id: new Date().toISOString(),
+  //       time: Date.now(),
+  //       storeCode: '65372c3c0df256000102683b',
+  //       tableId: '101',
+  //       tableName: '21',
+  //       orderInfo: [
+  //         {
+  //           pos_code: 'C00005',
+  //           good_qty: 1,
+  //           order_options: [
+  //             {
+  //               pos_code: 'C00003',
+  //               good_qty: 1,
+  //             },
+  //           ],
+  //         },
+  //         {
+  //           pos_code: 'C00005',
+  //           good_qty: 1,
+  //           order_options: [
+  //             {
+  //               pos_code: 'C00003',
+  //               good_qty: 1,
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     };
+  //
+  //     db.put(doc)
+  //       .then(async (response: any) => {
+  //         console.log('New data added:', response);
+  //         // 데이터 조회 (Read)
+  //         return await db.get(response.id);
+  //       })
+  //       .catch((error: any) => {
+  //         console.error('Error:', error);
+  //       });
+  //   } catch (e) {
+  //     console.log('error: ', e);
+  //   }
+  // };
 
+  async function addData() {
+    try {
+      const db = new PouchDB('test'); // 로컬 PouchDB 데이터베이스 생성
+      const remoteDB = new PouchDB('http://admin:0000@192.168.0.14:5984/test');
+
+      // DB 연결 및 동기화
       db.sync(remoteDB, {
         live: true,
         retry: true,
@@ -28,23 +89,53 @@ export default function HomePage() {
       // 데이터 추가 (Create)
       const doc = {
         _id: new Date().toISOString(),
-        title: 'local test',
-        content: 'This is some content.',
+        time: Date.now(),
+        storeCode: '65372c3c0df256000102683b',
+        tableId: '101',
+        tableName: '21',
+        orderInfo: [
+          {
+            pos_code: 'C00005',
+            good_qty: 1,
+            order_options: [
+              {
+                pos_code: 'C00003',
+                good_qty: 1,
+              },
+            ],
+          },
+          {
+            pos_code: 'C00005',
+            good_qty: 1,
+            order_options: [
+              {
+                pos_code: 'C00003',
+                good_qty: 1,
+              },
+            ],
+          },
+        ],
       };
 
-      db.put(doc)
-        .then(async (response: any) => {
-          console.log('New data added:', response);
-          // 데이터 조회 (Read)
-          return await db.get(response.id);
-        })
-        .catch((error: any) => {
-          console.error('Error:', error);
-        });
+      const response = await db.put(doc);
+      console.log('New data added:', response);
+
+      // 데이터 조회 (Read)
+      return await db.get(response.id);
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  }
+
+  function handleClickEvent() {
+    console.log('handleClick!');
+    try {
+      addData().then((r) => console.log(r));
     } catch (e) {
       console.log('error: ', e);
     }
-  };
+  }
 
   return (
     <S.HomeContainer>
