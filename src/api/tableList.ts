62ccff9dab6c728526ea79708681fcfa58de3b03
Daisy-khj5 <133727991+toder-khj5@users.noreply.github.com>
2023-11-08 16:03:38 +0900

    [edit] upload file

---
+++ src/api/tableList.ts
@@ -0,0 +1,32 @@
+import { type Table } from '@type/table.ts';
+
+export const TableList: Table[] = [
+  {
+    id: '1',
+    name: 'test-table',
+  },
+  {
+    id: '2',
+    name: 'test-table',
+  },
+  {
+    id: '3',
+    name: 'test-table',
+  },
+  {
+    id: '4',
+    name: 'test-table',
+  },
+  {
+    id: '5',
+    name: 'test-table',
+  },
+  {
+    id: '6',
+    name: 'test-table',
+  },
+  {
+    id: '7',
+    name: 'test-table',
+  },
+];
