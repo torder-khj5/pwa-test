082676b3d7bf355dfb6f6131a3b45c01feb8409a
Daisy-khj5 <133727991+toder-khj5@users.noreply.github.com>
2023-12-01 11:46:27 +0900

    [feat] 테이블번호 눌렀을때 /로 이동

--- src/components/@shared/Sidebar/index.tsx
+++ src/components/@shared/Sidebar/index.tsx
@@ -2,14 +2,17 @@ import { useTableSelector } from '@store/useTableStore.ts';
 import Icon from '@components/@shared/Icon';
 import Category from '@components/@shared/Category';
 import * as S from './styles.tsx';
+import { useNavigate } from 'react-router-dom';
 
 export default function Sidebar() {
   const { selectTable } = useTableSelector(['selectTable']);
+  const navigate = useNavigate();
+
   return (
     <S.SidebarContainer>
       <S.HeaderLogoContainer>
         <Icon name="로고_티오더_이름_흰색" width={80} height={60} />
-        <S.HeaderTableName>
+        <S.HeaderTableName onClick={() => navigate('/')}>
           <div style={{ color: 'white', fontSize: '20px' }}>{selectTable}번</div>
         </S.HeaderTableName>
       </S.HeaderLogoContainer>
